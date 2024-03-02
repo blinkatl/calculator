bc21dc616d450c398a26f622f4505d504e5e4a49
Daniel Balolong <daniel_balolong@sbcglobal.net>
2024-03-01 20:16:49 -0800

    Num buttons function to display

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 let operandOne;
 let operandTwo;
 let operator;
+let displayValue = document.querySelector(".display");
+let numBtn = document.querySelectorAll(".numBtn");
+let operandBtn = document.querySelector(".right button");
 
 const add = (a, b) => a + b;
-const subtract = (a, b) = a - b;
-const multiply = (a, b) = a * b;
-const divide = (a, b) = a / b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
 function operate(operandOne, operator, operandTwo) {
     switch(operator) {
@@ -19,3 +22,8 @@ function operate(operandOne, operator, operandTwo) {
             return divide(operandOne, operandTwo);
     }
 }
+numBtn.forEach(numBtn => {
+    numBtn.addEventListener("click", () => {
+        displayValue.textContent += numBtn.textContent;
+    });
+});
